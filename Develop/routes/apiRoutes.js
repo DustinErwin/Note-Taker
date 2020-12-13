cadef3da68991ce6c240415c1c662b2cd2cc632e
Galahad <derwin499@gmail.com>
2020-12-12 21:10:46 -0500

    deletetion functionality added

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,20 +3,37 @@ const fs = require("fs");
 const noteList = require("../db/db");
 
 router.get("/notes", (req, res) => {
-  res.json(noteList);
+  res.send(noteList);
 });
 
 router.post("/notes", (req, res) => {
-  const note = { title: req.body.title, text: req.body.text };
+  const note = {
+    title: req.body.title,
+    text: req.body.text,
+    id: Math.floor(Math.random() * 100000),
+  };
   noteList.push(note);
   fs.writeFile("./db/db.json", JSON.stringify(noteList), {}, (e) =>
     console.log(e)
   );
-  console.log(noteList);
 });
 
 router.delete("/notes/:id", (req, res) => {
-  res.json(noteList);
+  const noteId = parseInt(req.params.id);
+  let newNoteList = [];
+  console.log(noteList[1].id);
+  console.log(noteId);
+  for (let i = 0; i < noteList.length; i++) {
+    if (noteList[i].id !== noteId) {
+      newNoteList.push(noteList[i]);
+    }
+  }
+
+  fs.writeFile("./db/db.json", JSON.stringify(newNoteList), {}, (e) =>
+    console.log(e)
+  );
+
+  console.log(newNoteList);
 });
 
 module.exports = router;
